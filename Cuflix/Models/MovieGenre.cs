b60713d3008503d2e986855785bb2d6c95d66fb1
Catisinhaflix <luacoutinhor@gmail.com>
2023-05-16 08:46:55 -0300

    modificação do Movie.cs e criação e modificação do MovieGenre.cs

---
+++ Cuflix/Models/MovieGenre.cs
@@ -0,0 +1,10 @@
+namespace Cuflix.Models;
+public class MovieGenre
+{
+     public int MovieId { get; set; }
+
+     public Movie Movie { get; set; }
+
+     public byte GenreId { get; set; }
+     public Genre Genre { get; set; }
+}
